2bf2e35c4f3b9aeb62b3807a636025b7c9125014
Brrn91 <lucasbruno81@gmail.com>
2026-04-16 10:06:58 -0300

    feat: update login and password recovery functionality with improved button interactions and modal handling

--- scripts/esqueci-a-senha.js
+++ scripts/esqueci-a-senha.js
@@ -1,11 +1,29 @@
-const botaoModal = document.querySelector(".botao-modal");
 const modalFundo = document.querySelector(".modal-fundo");
-const botaoFechar = document.querySelector(".botao-fechar");
+function esqueceuASenha() {
+  const botaoModal = document.querySelector(".botao-modal");
+  const botaoFechar = document.querySelector(".botao-fechar");
 
-botaoModal.addEventListener("click", function () {
-  modalFundo.classList.add("modal-aberto");
-});
+  botaoModal.addEventListener("click", function () {
+    modalFundo.classList.add("modal-aberto");
+  });
 
-botaoFechar.addEventListener("click", function () {
-  modalFundo.classList.remove("modal-aberto");
-});
+  botaoFechar.addEventListener("click", function () {
+    modalFundo.classList.remove("modal-aberto");
+  });
+}
+
+esqueceuASenha();
+
+// Envia um alerta de sucesso ao redefinir a senha
+
+function redefinirSenha(event) {
+  if (document.getElementById("email-recuperacao").value === "") {
+    alert("Por favor, preencha o campo de e-mail.");
+  } else {
+    event.preventDefault();
+    alert(
+      `E-mail de recuperação enviado com sucesso para ${document.getElementById("email-recuperacao").value}!`,
+    );
+    modalFundo.classList.remove("modal-aberto");
+  }
+}
